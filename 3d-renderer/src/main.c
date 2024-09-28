200498c9f38418dc010fae423907a06de661e212
Codebrudi <codebrudi@pm.me>
2024-09-28 16:10:49 +0200

    show red window

--- 3d-renderer/src/main.c
+++ 3d-renderer/src/main.c
@@ -1,7 +1,81 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
 
-int main(void) {
-	printf("Hello, world!\n");
-	return 0;
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL.\n");
+		return false;
+	}
+
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		600,
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window.\n");
+		return false;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer.\n");
+		return false;
+	}
+	return true;
 }
 
+void setup(void) {
+
+}
+
+void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type)	{
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = false;
+			break;	
+	}
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 1); //RGBA
+	SDL_RenderClear(renderer);
+
+	// ..
+
+	SDL_RenderPresent(renderer);
+}
+
+
+int main(void) {
+	is_running = initialize_window();
+
+	setup();
+
+	while(is_running) {
+		process_input();
+		update();
+		render();
+	}
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -6,7 +6,9 @@ SDL
 
 ### installation
 
-macos: brew install sdl2
+macos:
+  brew install sdl2
+  brew install pkg-config
 linux:  
   sudo apt install build-essential
   sudo apt install libsdl2-dev
