b31ab4045a9e58089415301ec4c5e4305be96fbf
Codebrudi <codebrudi@pm.me>
2024-10-02 23:01:40 +0200

    lock game loop to 30 fps

--- 3d-renderer/src/main.c
+++ 3d-renderer/src/main.c
@@ -15,6 +15,7 @@ vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
 float fov_factor = 640;
 
 bool is_running = false;
+int previus_frame_time = 0;
 
 void setup(void) {
 	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
@@ -68,9 +69,14 @@ vec2_t project(vec3_t point) {
 }
 
 void update(void) {
-	cube_rotation.x += 0.001;
-	cube_rotation.y += 0.001;
-	cube_rotation.z += 0.001;
+
+	while (SDL_TICKS_PASSED(SDL_GetTicks(), previus_frame_time + FRAME_TARGET_TIME) == false);
+
+	previus_frame_time = SDL_GetTicks();
+
+	cube_rotation.x += 0.01;
+	cube_rotation.y += 0.01;
+	cube_rotation.z += 0.01;
 
 	for (int i = 0; i < N_POINTS; i++) {
 		vec3_t point = cube_points[i];
