b1c7a03b46b45441eeb69983d2a05cb014f3cf7a
Codebrudi <codebrudi@pm.me>
2024-10-06 16:15:48 +0200

    add vector math

--- 3d-renderer/src/data-structures/vector.c
+++ 3d-renderer/src/data-structures/vector.c
@@ -30,3 +30,97 @@ vec3_t vec3_rotate_z(vec3_t v, float angle)
     };
     return rotated_vector;
 }
+
+float vec3_get_magnitude(vec3_t v) {
+    return sqrtf(powf(v.x, 2) + powf(v.y, 2)+ powf(v.z, 2));
+}
+
+float vec2_get_magnitude(vec2_t v) {
+    vec3_t v3 = { .x = v.x, .y = v.y, .z = 0 };
+    return vec3_get_magnitude(v3);
+}
+
+vec3_t vec3_add(vec3_t a, vec3_t b) {
+    vec3_t result = {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z
+    };
+    return result;
+}
+
+vec2_t vec2_add(vec2_t a, vec2_t b) {
+    vec2_t result = {
+        .x = a.x + b.x,
+        .y = a.y + b.y
+    };
+    return result;
+}
+
+vec3_t vec3_subtract(vec3_t a, vec3_t b) {
+    vec3_t result = {
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+        .z = a.z - b.z
+    };
+    return result;
+}
+
+vec2_t vec2_subtract(vec2_t a, vec2_t b) {
+    vec2_t result = {
+        .x = a.x - b.x,
+        .y = a.y - b.y
+    };
+    return result;
+}
+
+vec3_t vec3_multiply(vec3_t v, float factor) {
+    vec3_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor,
+        .z = v.z * factor
+    };
+    return result;
+}
+
+vec2_t vec2_multiply(vec2_t v, float factor) {
+    vec2_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor
+    };
+    return result;
+}
+
+vec3_t vec3_divide(vec3_t v, float factor) {
+    vec3_t result = {
+        .x = v.x / factor,
+        .y = v.y / factor,
+        .z = v.z / factor
+    };
+    return result;
+}
+
+vec2_t vec2_divide(vec2_t v, float factor) {
+    vec2_t result = {
+        .x = v.x / factor,
+        .y = v.y / factor
+    };
+    return result;
+}
+
+vec3_t vec3_cross_product(vec3_t a, vec3_t b) {
+    vec3_t result = {
+        .x = (a.y * b.z) - (a.z * b.y),
+        .y = (a.z * b.x) - (a.x * b.z),
+        .z = (a.x * b.y) - (a.y * b.x)
+    };
+    return result;
+}
+
+float vec3_dot_product(vec3_t a, vec3_t b) {
+    return (a.x * b.x) + (b.x * b.y) + (a.z * b.z);
+}
+
+float vec2_dot_product(vec2_t a, vec2_t b) {
+    return (a.x * b.x) + (b.x * b.y);
+}
--- 3d-renderer/src/data-structures/vector.h
+++ 3d-renderer/src/data-structures/vector.h
@@ -18,4 +18,24 @@ vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
 
+float vec3_get_magnitude(vec3_t v);
+float vec2_get_magnitude(vec2_t v);
+
+vec3_t vec3_add(vec3_t a, vec3_t b);
+vec2_t vec2_add(vec2_t a, vec2_t b);
+
+vec3_t vec3_subtract(vec3_t a, vec3_t b);
+vec2_t vec2_subtract(vec2_t a, vec2_t b);
+
+vec3_t vec3_multiply(vec3_t v, float factor);
+vec2_t vec2_multiply(vec2_t v, float factor);
+
+vec3_t vec3_divide(vec3_t v, float factor);
+vec2_t vec2_divide(vec2_t v, float factor);
+
+vec3_t vec3_cross_product(vec3_t a, vec3_t b);
+
+float vec3_dot_product(vec3_t a, vec3_t b);
+float vec2_dot_product(vec2_t a, vec2_t b);
+
 #endif
